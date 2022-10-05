b3e533c4ec9fd912518e052421dd7ead4c5c53e7
Shubham Bairagi <sbairagi825@gmail.com>
2022-10-05 16:11:21 +0530

    deploy on heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/base-app/'));
+app.use(express.static(__dirname + '/dist/dcart/'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/base-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dcart/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
